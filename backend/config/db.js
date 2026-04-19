3177ff9eef82808cf6d9928018e12b61edc6de8b
owaisnaeem-coding <0hussainnaeem786@gmail.com>
2026-04-19 05:10:46 +0500

    all done

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,8 +7,8 @@ const connectDB = async () => {
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error.message);
-    process.exit(1);
-  }
+      process.exit(1);
+    }
 };
 
 module.exports = connectDB;
